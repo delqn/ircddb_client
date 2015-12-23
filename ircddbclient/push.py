b24569afa7f035a99545bcaffa6b4e1e402f91d8
Delyan Raychev <delyanr@gmail.com>
2015-12-23 15:52:27 -0800

    MIN_TIME_BETWEEN_IRCDDB_PULLS

--- ircddbclient/push.py
+++ ircddbclient/push.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-import json
 import httplib
+import json
 import urllib
 
 import ConfigParser
--- run_service.py
+++ run_service.py
@@ -4,13 +4,19 @@ import time
 
 from ircddbclient import Push, Pull
 
-SLEEP_SECONDS = 20
+MIN_TIME_BETWEEN_IRCDDB_PULLS = 30
 
 if __name__ == '__main__':
     puller = Pull()
     pusher = Push(class_name='ParsedStream')
+
     while True:
+        start_time = time.time()
         for message in puller.pull():
             print message
-            pusher.push(data=message)
-        time.sleep(SLEEP_SECONDS)
+            pusher.push(message)
+
+        # take it easy on the ircDDB system
+        elapsed_time = time.time() - start_time
+        if elapsed_time < MIN_TIME_BETWEEN_IRCDDB_PULLS:
+            time.sleep(MIN_TIME_BETWEEN_IRCDDB_PULLS - elapsed_time)
