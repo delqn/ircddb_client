54a8a73cd1dce7de6e1eefa0dd5d92521bacc98f
Delyan Raychev <delyanr@gmail.com>
2015-12-23 12:06:57 -0800

    Cleanup

--- run_service.py
+++ run_service.py
@@ -6,20 +6,11 @@ from ircddbclient import Parser, PushMessage, PullMessages
 
 SLEEP_SECONDS = 20
 
-
-def push_message(msg):
-    PushMessage(class_name='Stream', data={'Message': msg})()
-
-
-def push_parsed_message(data_):
-    PushMessage(class_name='ParsedStream', data=data_)()
-
 if __name__ == '__main__':
     puller = PullMessages()
     parser = Parser()
     while True:
         for message in puller.pull():
             print message
-            # threading.Thread(target=push_message, args=(message,)).start()
-            push_parsed_message(message)
+            PushMessage(class_name='ParsedStream', data=message)()
         time.sleep(SLEEP_SECONDS)
