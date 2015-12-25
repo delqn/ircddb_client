65d28da4764b3bc959a42f3e6949326677dff982
Delyan Raychev <delyanr@gmail.com>
2015-12-25 10:10:01 -0800

    Command line option to enable/disable multiprocessing

--- run_service.py
+++ run_service.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import time
 
 from multiprocessing import Pool
@@ -14,12 +15,20 @@ def push(message):
     Push(class_name='ParsedStream').push(message)
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--multiprocessing', type=bool, default=False,
+                        help='Enable multiprocessing. Disabled by default.')
+    args = parser.parse_args()
     puller = Pull()
     while True:
         start_time = time.time()
-        pool = Pool(5)
         messages = puller.pull()
-        pool.map(push, messages)
+        if args.multiprocessing:
+            pool = Pool(5)
+            pool.map(push, messages)
+        else:
+            for msg in messages:
+                push(msg)
 
         # take it easy on the ircDDB system
         elapsed_time = time.time() - start_time
